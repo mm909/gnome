6a70661f0d1fe7ce19c7faa634b291c6edbf46d6
Janushan-Austin <38991541+Janushan-Austin@users.noreply.github.com>
2020-01-23 09:33:01 -0800

    added simple pathing

--- main.py
+++ main.py
@@ -21,7 +21,19 @@ def getStartPoint():
                 startY = i
                 pygame.draw.rect(win, (255,255,255), (j * 32 + 12, i * 32 + 12, 8, 8))
                 return startX, startY
-    return
+    return startX, startY
+
+def findClusterPoint(): #will be used to find the next room to create a path too. the same as getStartPoint just didnt want to rename that function and change it a little without discussing first
+    startX = -1
+    startY = -1
+    for i, backgroundRow in enumerate(background):
+        for j, tile in enumerate(backgroundRow):
+            if background[i][j] == 0 and not (j , i) in visitedBackground:
+                startX = j
+                startY = i
+                pygame.draw.rect(win, (255,255,255), (j * 32 + 12, i * 32 + 12, 8, 8))
+                return startX, startY
+    return startX, startY
 
 def connected(x,y):
     visitedBackground.append((x,y))
@@ -30,12 +42,13 @@ def connected(x,y):
             neighborX = x + i
             if neighborX >= 0 and neighborX < tilesX:
                 for j in range (-1, 2):
-                    neighborY = y + j
-                    if neighborY >= 0 and neighborY < tilesY:
-                        if background[neighborY][neighborX] == 0 and not (neighborX,neighborY) in visitedBackground:
-                            if not (i == 0 and j == 0):
-                                pygame.draw.rect(win, (100,255,100), (neighborX * 32 + 12, neighborY * 32 + 12, 8, 8))
-                                connected(neighborX, neighborY)
+                    if i == 0 or j == 0:   #only does dfs on non-diagonal neighbors to stop diagonal only path connections if  (abs(i) != 1 or abs(j) != 1):
+                        neighborY = y + j
+                        if neighborY >= 0 and neighborY < tilesY:
+                            if background[neighborY][neighborX] == 0 and not (neighborX,neighborY) in visitedBackground:
+                                if not (i == 0 and j == 0):
+                                    pygame.draw.rect(win, (100,255,100), (neighborX * 32 + 12, neighborY * 32 + 12, 8, 8))
+                                    connected(neighborX, neighborY)
     return
 
 def createBackground():
@@ -60,6 +73,56 @@ def createBackground():
                         background[roomY + j][roomX + k] = 0
     return background
 
+def connectRooms():
+    startX, startY = getStartPoint()
+    connected(startX, startY)
+    endX, endY = findClusterPoint()
+    while endX != -1 and endY != -1:
+        #connected(endX, endY)
+        createPath(startX, startY, endX, endY)
+        endX, endY = findClusterPoint()
+    return
+
+def createPath(startX, startY, endX, endY):
+    global visitedBackground
+    dx = 0
+    dy = 0
+
+    slopeX = abs(startX - endX)
+    slopeY = abs(startY - endY)
+
+    if startX - endX < 0:
+        dx = 1
+    elif startX - endX > 0:
+        dx = -1
+
+    if startY - endY < 0:
+        dy = 1
+    elif startY - endY > 0:
+        dy = -1
+
+    #print(endX, endY)
+    moveX = True
+    currX = startX
+    currY = startY
+    while not (endX, endY) in visitedBackground:
+        if slopeX > 0 and moveX == True:
+            currX = currX + dx
+            background[currY][currX] = 0
+            slopeX = slopeX - 1
+            visitedBackground = []
+            connected(startX, startY)
+        elif slopeY > 0 and moveX == False:
+            currY = currY + dy
+            background[currY][currX] = 0
+            slopeY = slopeY - 1
+            visitedBackground = []
+            connected(startX, startY)
+
+        moveX = not moveX
+
+    return
+
 def update():
     win.fill(background_colour)
     for i, backgroundRow in enumerate(background):
@@ -72,6 +135,8 @@ def update():
         pass
     startX, startY = getStartPoint()
     connected(startX, startY)
+
+    visitedBackground = []
     pygame.display.flip()
     return 1
 
@@ -94,6 +159,8 @@ wall = pygame.image.load("map/stone2_gray0.png").convert()
 background = createBackground()
 visitedBackground = []
 
+connectRooms()
+
 pygame.display.flip()
 running = True
 while running:
@@ -103,6 +170,7 @@ while running:
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_F1:
                 background = createBackground()
+                connectRooms()
             if event.key == pygame.K_f:
                 switchWallStatus = not switchWallStatus
         if pygame.mouse.get_pressed()[0]:
