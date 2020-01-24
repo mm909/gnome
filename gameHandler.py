b2d26f774ee056a7ec67aa0d2c55132574572047
Mikian Musser <Bobar312@gmail.com>
2020-01-24 15:59:07 -0800

    Incresses scroll range

--- gameHandler.py
+++ gameHandler.py
@@ -54,20 +54,14 @@ class GameHandler:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 spriteResized = False
                 if event.button == 4:
-                    self.spriteSize = min(self.spriteSize + 2, 56)
+                    self.spriteSize = min(self.spriteSize + 2, 64)
                     spriteResized = True
-                    #self.map.setSpriteSize(self.spriteSize)
-                    #print(self.spriteSize)
                 if event.button == 5:
-                    self.spriteSize = max(self.spriteSize - 2, 8)
+                    self.spriteSize = max(self.spriteSize - 2, 2)
                     spriteResized = True
-                    #self.map.setSpriteSize(self.spriteSize)
-                    #print(self.spriteSize)
-
                 if spriteResized == True: #just making it easier if for some reason we have to add more function calls we dont just copy and paste lines
                     print(self.spriteSize)
                     self.map.setSpriteSize(self.spriteSize)
-
 
             if pygame.mouse.get_pressed()[0]:
                 self.map.switchWall()
