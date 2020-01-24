5032f9f9d96a5c126da9a0a925235300afcd721b
Janushan-Austin <38991541+Janushan-Austin@users.noreply.github.com>
2020-01-24 15:33:15 -0800

    sprites now keep a copy of the original loaded image for scaling

--- sprite.py
+++ sprite.py
@@ -7,15 +7,17 @@ class Sprite:
         self.zoom = zoom
         self.filename = filename
         self.art = pygame.image.load(filename).convert()
-
+        self.originalArt = self.art #used to keep a copy of the original so when scaling we do not have to reload the image
     def draw(self, window, pos):
         window.blit(self.art, pos)
 
     # This will be used for zoom (we only hav to resize sprites every time zoom happens)
     def resize(self, size):
         self.size = size
-        if size == 32:
-            self.art = pygame.image.load(self.filename).convert()
-        else:
-            self.art = pygame.image.load(self.filename).convert() # loading everytime seems to help with image quality since we arent scaling an already scaled image again now
-            self.art = pygame.transform.scale(self.art, (self.size, self.size))
+        # if size == 32:
+        #     self.art = self.originalArt
+        # else:
+        #     #self.art = pygame.image.load(self.filename).convert() # loading everytime seems to help with image quality since we arent scaling an already scaled image again now
+        #     self.art = pygame.transform.scale(self.originalArt, (self.size, self.size))
+        self.art = pygame.transform.scale(self.originalArt, (self.size, self.size))
+        return
